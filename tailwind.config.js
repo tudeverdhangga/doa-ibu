521e810d00dc11da049682a54cc16feb154d23f0
tudeverdhangga <tudeverdhangga@gmail.com>
2022-03-25 00:09:24 +0800

    Install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}, './node_modules/tw-elements/dist/js/**/*.js'",
+  ],
   theme: {
     extend: {},
   },
