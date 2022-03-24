10b52370aea63734ef0acfea7b25c20262cb43fc
satriyoaji27 <satriyoaji27@gmail.com>
2022-03-24 20:08:43 +0700

    fix unrendered

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
+  Route,
 } from "react-router-dom";
 import { APP_ROUTE } from "./routes/route";
 import logo from './logo.svg';
 import './App.css';
-import PublicRoute from "./components/PublicRoute";
+import AppContainer from "./components/AppContainer";
+import AboutPage from "./pages/about";
+import AcademicPage from "./pages/academic";
 
 function App() {
   return (
     <Router>
-    {APP_ROUTE.map((value, index) => {
-      return (
-        <PublicRoute
-          key={value.name}
-          restricted={value.restricted}
-          path={value.path}
-          component={value.component}
-          exact={value.exact}
-          isNotFound={value.isNotFound}
-        />
-      );
-    })}
+      <Routes>
+          <Route path="/" element={<AppContainer />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/academic" element={<AcademicPage />} />
+        {/*{APP_ROUTE.map((value, index) => {*/}
+        {/*  return (*/}
+        {/*    <PublicRoute*/}
+        {/*      key={value.name}*/}
+        {/*      restricted={value.restricted}*/}
+        {/*      path={value.path}*/}
+        {/*      component={value.component}*/}
+        {/*      exact={value.exact}*/}
+        {/*      isNotFound={value.isNotFound}*/}
+        {/*    />*/}
+        {/*  );*/}
+        {/*})}*/}
+      </Routes>
     </Router>
   );
 }
