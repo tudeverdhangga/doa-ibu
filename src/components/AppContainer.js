5127485a43fc1546c61809b871e92a10a635b38c
ekkyregita <ekky.shoesmart@gmail.com>
2022-03-24 22:34:58 +0700

    INSTALL TAILWIND

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -3,7 +3,7 @@ import { FaHome, IconName } from "react-icons/fa";
 import NavBar from "./NavBar";
 import FooterBar from "./FooterBar";
 import { useLocation, Redirect } from "react-router-dom";
-import {Button} from "@mui/material";
+import NewsSection from "./news-section";
 
 const AppContainer = (props) => {
   const location = useLocation();
@@ -25,9 +25,9 @@ const AppContainer = (props) => {
   };
 
   return (
-    <div>
-      <h1>Content</h1>
-    </div>
+    <>
+      <NewsSection />
+    </>
   );
 };
 
