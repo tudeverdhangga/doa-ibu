43bf47492e985c1b50593d6eb8b7a9b119de3260
satriyoaji27 <satriyoaji27@gmail.com>
2022-03-25 22:15:24 +0700

    ongoing about page

---
+++ src/dump/aboutContent.js
@@ -0,0 +1,37 @@
+export const Widgets = [
+    {
+        "img": "akses_kampus.jpeg",
+        "body": "Akses menuju ke PENS"
+    },
+    {
+        "img": "formasi-penerimaan.png",
+        "body": "Surat resmi formasi penerimaan PNS di PENS"
+    },
+    {
+        "img": "legalisir.jpeg",
+        "body": "Legalisir resmi dan transkip"
+    },
+    {
+        "img": "pegawai_honorer.jpg",
+        "body": "Pegawai Honorer PENS tahun terakhir"
+    },
+    {
+        "img": "si_mila.jpeg",
+        "body": "Sistem Informasi Masukan, Informasi Layanan dan Pengaduan"
+    },
+    {
+        "img": "survey_kepuasan.jpeg",
+        "body": "Survey Kepuasan Informasi yang Disajikan di Website PENS"
+    }
+]
+
+export const Explanation = {
+    "body": "Awal sejarah PENS dimulai pada tahun 1985. Saat itu, tim studi awal Japan International Cooperation Agency (JICA) untuk bantuan dan kerjasama teknik yang dikepalai oleh Prof. Y. Naito dari Tokyo Institute of Technology, datang ke politeknik ini. Setelah melakukan pengamatan dan studi kelayakan di tahun 1986, JICA menyetujui untuk memulai kerjasama teknik di tahun 1987 dengan mengirim 5 orang pengajar Indonesia ke perguruan tinggi teknologi di Jepang.",
+    "visi": "Menjadi pusat unggulan pendidikan teknologi rekayasa di bidang emerging technology dalam skala nasional maupun internasional.",
+    "misi": [
+        "Menyelenggarakan pendidikan dengan menyediakan lingkungan dan suasana akademik yang berkualitas untuk menghasilkan lulusan yang profesional, berpikiran terbuka, kreatif dan berjiwa pemimpin, yang siap bersaing di era global.",
+        "Sebagai sumber daya politeknik nasional, berperan aktif dalam pengembangan dan peningkatan sistem pendidikan politeknik di Indonesia.",
+        "Melaksanakan penelitian yang berorientasi penemuan, pengembangan, kombinasi, atau integrasi dari beberapa teknologi yang sudah ada sebelumnya, menjadi teknologi baru yang membawa kemaslahatan masyarakat.",
+        "Membangun dan mengimplementasikan nilai-nilai etika moral akademis dan sosial masyarakat."
+    ]
+}
