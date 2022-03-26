e1b32b16245deffc73c7540b000d7b4e1ed8b227
tudeverdhangga <tudeverdhangga@gmail.com>
2022-03-27 00:12:41 +0800

    Animation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import AcademicPage from "./pages/academic";
 import NavBar from "./components/NavBar";
 import FooterBar from "./components/FooterBar";
 import NewsDetailPage from "./pages/news";
+// import 'animate.css/animate.css'
+import 'animate.css/animate.min.css'
 
 function App() {
   return (
