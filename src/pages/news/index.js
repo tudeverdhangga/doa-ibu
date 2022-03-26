4ba84214dee5d9950144412eb58d36b7131506ff
satriyoaji27 <satriyoaji27@gmail.com>
2022-03-27 06:58:01 +0700

    make autoscroll and generator for news detail

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {Link, useParams} from "react-router-dom";
 import {url} from "tailwindcss/lib/util/dataTypes";
+import AutoScroll from '@brianmcallister/react-auto-scroll';
 import news from "../../dump/news.json"
 
 NewsDetailPage.propTypes = {};
 
 function NewsDetailPage(props) {
     const [newsDetail, setNewsDetail] = React.useState(null);
+    const [newsNewest, setNewsNewest] = React.useState([]);
+    const [time, setTime] = React.useState(Date.now());
 
     let { id } = useParams();
 
@@ -14,18 +17,41 @@ function NewsDetailPage(props) {
         if (Array.isArray(news.news)) {
             setNewsDetail(news.news[id-1])
         }
-    }, []);
+    }, [id]);
+
+    React.useEffect(() => {
+        if (Array.isArray(news.news)) {
+            setNewsNewest(news.news)
+        }
+        const interval = setInterval(() => {
+            setTime(Date.now())
+            if (Math.random() > 0.5) {
+                return;
+            }
+            if (Array.isArray(news.news)) {
+                if(newsNewest.length%7 == 0) {
+                    newsNewest.push(news.news[0])
+                } else {
+                    newsNewest.push(news.news[(newsNewest.length%7) + 1])
+                }
+                setNewsNewest(newsNewest)
+            }
+            // setNewsNewest(newsNewest.concat([msg.join(' ')]));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [newsNewest]);
 
     return (
         <>
             <div className="grid lg:grid-cols-12 gap-4 justify-between mt-6">
-                <div className="lg:col-span-10 grid grid-rows-1 justify-center p-4 mx-6">
-                    <section id="newsHeader" className="flex flex-wrap justify-center p-6">
+                <div className="xl:col-span-10 md:col-span-9 grid grid-rows-1 justify-center p-4 md:mx-6 xs:mx-3">
+                    <section id="newsHeader" className="grid grid-rows-1 flex justify-center p-6">
                         <div className="text-justify">
                             <h1 className="text-4xl font-semibold">{newsDetail !== null ? newsDetail.header?.title : ""}</h1>
                             <p className="text-md my-3">{newsDetail !== null ? "by "+newsDetail.header?.editor + " | " + newsDetail.header?.date: ""}</p>
                         </div>
-                        <div className="mt-6 p-0">
+                        <div className="mt-6 p-0 flex justify-center">
                             <img
                                 className="block max-w-full h-auto news-header"
                                 src={newsDetail !== null ? `/asset/${newsDetail.img}` : `/asset/news_cartoon.jpg`}
@@ -38,22 +64,21 @@ function NewsDetailPage(props) {
                         <p className="text-md text-justify">{newsDetail !== null ? newsDetail.body : ""}</p>
                     </section>
                 </div>
-                <div className="lg:col-span-2 mx-6 p-4">
+                <div className="xl:col-span-2 md:col-span-3 md:mx-6 xs:mx-2 p-4">
                     <h3 className="text-xl text-center">Berita Terbaru</h3>
-                    <div className="newest-news">
+                    <div className="">
+                        <AutoScroll height={500} scrollBehavior={`smooth`} optionText={`Auto scroll`}>
                         {
-                            Array.isArray(news.news) ?
-                                news.news.map((value, idx) => {
+                            Array.isArray(newsNewest) ?
+                                newsNewest.map((value, idx) => {
                                     return (
                                         <div key={idx} className="mt-12 flex justify-center">
                                             <Link
-                                                to={`/news/3`}
+                                                to={`/news/${value.id}`}
                                             >
                                                 <div className="rounded-lg shadow-lg bg-white max-w-sm text-justify">
-                                                    <a href="#!">
-                                                        <img className="rounded-t-lg" src={`/asset/${value.img}`}
-                                                             alt=""/>
-                                                    </a>
+                                                    <img className="rounded-t-lg" src={`/asset/${value.img}`}
+                                                         alt=""/>
                                                     <div className="p-6">
                                                         <h5 className="text-gray-900 text-sm font-medium">{value.header.title}</h5>
                                                     </div>
@@ -63,6 +88,7 @@ function NewsDetailPage(props) {
                                     )
                                 }) : ''
                         }
+                        </AutoScroll>
                     </div>
                 </div>
             </div>
