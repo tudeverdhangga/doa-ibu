e48508300e56a00114f480907d8a0631150fbe5e
tudeverdhangga <tudeverdhangga@gmail.com>
2022-03-28 00:27:15 +0800

    Fix UI about & news detail

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,122 +10,99 @@ AboutPage.propTypes = {};
 function AboutPage(props) {
     return (
         <>
-        <div className="text-center relative overflow-hidden bg-no-repeat bg-cover about-header">
-            <div
-                className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                style={{backgroundColor:"rgba(0, 0, 0, 0.6)"}}
-            >
-                <div className="flex justify-center items-center h-full">
-                    <div className="text-white xl:px-56 px-10">
-                        <h1 className="font-roboto text-3xl font-bold">Tentang Politeknik Elektronika Negeri Surabaya</h1>
-                        <h5 className="text-xl pt-2 font-roboto">Politeknik Elektronika Negeri Surabaya adalah kampus modern, komprehensif, terbuka, multi budaya, dan humanis yang mencakup disiplin ilmu yang luas.</h5>
+            <div className="text-center relative overflow-hidden bg-no-repeat bg-cover about-header">
+                <div
+                    className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+                    style={{backgroundColor:"rgba(0, 0, 0, 0.6)"}}
+                >
+                    <div className="flex justify-center items-center h-full">
+                        <div className="text-white xl:px-56 px-10">
+                            <h1 className="font-roboto text-3xl font-bold">Tentang Politeknik Elektronika Negeri Surabaya</h1>
+                            <h5 className="text-xl pt-2 font-roboto">Politeknik Elektronika Negeri Surabaya adalah kampus modern, komprehensif, terbuka, multi budaya, dan humanis yang mencakup disiplin ilmu yang luas.</h5>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="md:px-12 xs:px-6 py-10">
-            <div className="px-12 mt-12 grid md:grid-cols-3 xs:grid-cols-1">
-                <div className="grid col-span-2 grid-rows-1 gap-4">
-                    <div className="">
+            <div className="px-20 xs:px-6 py-10 font-roboto">
+                <div className="px-12 mt-12">
+                    <div>
                         <div>
-                            <h2 className="font-semibold text-center text-3xl">Sejarah</h2>
-                        </div>
-                        <div className="mt-4 px-6">
-                            <p className="text-justify">{Explanation.body}</p>
-                        </div>
-                        <div className="mt-4 px-6 text-justify">
-                            {
-                                Explanation.body_history.map((value, idx) => {
-                                    return (
-                                        <div key={idx}>
-                                            <p className="font-semibold">
-                                                {value.subtitle}
-                                            </p>
-                                            <p className="mb-4">
-                                                {value.desc}
-                                            </p>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className={`mt-6`}>
-                            <h2 className="font-semibold text-center text-3xl">Visi</h2>
-                        </div>
-                        <div className="mt-4 px-6">
-                            <p className="text-justify">{Explanation.visi}</p>
-                        </div>
-                        <div className={`mt-6`}>
-                            <h2 className="font-semibold text-center text-3xl">Misi</h2>
-                        </div>
-                        <div className="mt-4 px-6 text-justify">
-                            <div className="flex justify-center">
-                                <ul className="bg-white rounded-lg text-gray-900">
-                                    {
-                                        Explanation.misi.map((value, idx) => {
-                                            return (
-                                                <li key={idx} className={"py-2 border-b border-gray-300 w-full rounded-t-lg"}>
-                                                    {value}
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid grid-rows-12 gap-4 justify-end">
-                    <div className="text-center">
-                        <h2 className="font-semibold text-3xl">Widgets</h2>
-                    </div>
-                    <div className="p-2 m-2 grid lg:grid-cols-2 xs:grid-cols-1 gap-4">
-                        {
-                            Widgets.map((value, index) => {
-                                return (
-                                    <div key={index} className="flex justify-center">
-                                        <div key={index} className="rounded-lg shadow-lg bg-white max-w-sm">
-                                            <a href="#!">
-                                                <img className="rounded-t-lg hover:scale-125" src={`/asset/${value.img}`}
-                                                     alt=""/>
-                                            </a>
-                                            <div className="p-6">
-                                                <h5 className="text-gray-900 text-xl font-medium mb-2">{`${value.body}`}</h5>
-                                                <p className="text-gray-700 text-base mb-4"></p>
-                                            </div>
-                                        </div>
+                            <div>
+                                <div className="inline-block mb-8 font-roboto">
+                                    <h3 className="w-[40%] mb-10 font-extrabold text-5xl realtive pt-[20px] pr-auto pb-[15px] border-b-4 border-b-[#f2c808] border-solid text-[#103968]">Sejarah Kampus</h3>
+                                    <div className="mt-4 px-6">
+                                        <p className="text-justify">{Explanation.body}</p>
                                     </div>
-                                );
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="my-6 container mx-auto bg-scroll content-bg-grey py-10 ">
-            <div>
-                <h2 className="font-semibold text-center text-3xl mb-5">Layanan</h2>
-            </div>
-            <div className="grid grid-rows-1 gap-4 px-12">
-            {
-                Services.map((value, idx) => {
-                    return (
-                        <div key={idx} className="flex mt-4 px-12">
-                            <div className="w-full flex flex-col md:flex-row rounded-lg bg-white shadow-lg">
-                                <img
-                                    className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg hover:rotate-[15deg]"
-                                    src={`/asset/${value.img}`} alt=""/>
-                                <div className="p-6 flex flex-col justify-start">
-                                    <h5 className="text-gray-900 text-xl font-medium mb-2">{`${value.title}`}</h5>
-                                    <p className="text-gray-700 text-base mb-4">{`${value.body}`}</p>
+                                    <div className="mt-4 px-6 text-justify">
+                                        {
+                                            Explanation.body_history.map((value, idx) => {
+                                                return (
+                                                    <div key={idx}>
+                                                        <p className="font-semibold">
+                                                            {value.subtitle}
+                                                        </p>
+                                                        <p className="mb-4">
+                                                            {value.desc}
+                                                        </p>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div>                        
+                            <div className={`mt-6`}>
+                                <h3 className="w-[10%] mb-3 font-extrabold text-5xl realtive pt-[20px] pr-auto pb-[15px] border-b-4 border-b-[#f2c808] border-solid text-[#103968]">Visi</h3>
+                            </div>
+                            <div className="mt-4 px-6">
+                                <p className="text-justify">{Explanation.visi}</p>
+                            </div>
+                            <div className={`mt-10`}>
+                                <h3 className="w-[10%] mb-3 font-extrabold text-5xl realtive pt-[20px] pr-auto pb-[15px] border-b-4 border-b-[#f2c808] border-solid text-[#103968]">Misi</h3>
+                            </div>
+                            <div className="mt-4 px-6 text-justify">
+                                <div className="flex justify-center">
+                                    <ul className="bg-white rounded-lg text-gray-900 list-disc">
+                                        {
+                                            Explanation.misi.map((value, idx) => {
+                                                return (
+                                                    <li key={idx} className={"py-2 w-full rounded-t-lg"}>
+                                                        {value}
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
                                 </div>
                             </div>
                         </div>
-                    )
-                })
-            }
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="my-10 mx-auto py-14 container content-bg-grey font-roboto">
+                <div>
+                    <h2 className="font-semibold text-center text-3xl mb-5">Layanan</h2>
+                </div>
+                <div className="grid grid-rows-1 gap-4 px-16">
+                {
+                    Services.map((value, idx) => {
+                        return (
+                            <div key={idx} className="flex mt-4 px-12">
+                                <div className="w-full flex flex-col md:flex-row rounded-lg bg-white shadow-lg">
+                                    <img
+                                        className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                                        src={`/asset/${value.img}`} alt=""/>
+                                    <div className="p-6 flex flex-col justify-start">
+                                        <h5 className="text-gray-900 text-xl font-medium mb-2">{`${value.title}`}</h5>
+                                        <p className="text-gray-700 text-base mb-4">{`${value.body}`}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -44,9 +44,9 @@ function NewsDetailPage(props) {
 
     return (
         <>
-            <div className="grid lg:grid-cols-12 gap-4 justify-between mt-6">
-                <div className="xl:col-span-10 md:col-span-9 grid grid-rows-1 justify-center p-4 md:mx-6 xs:mx-3">
-                    <section id="newsHeader" className="grid grid-rows-1 flex justify-center p-6">
+            <div className="grid lg:grid-cols-12 gap-4 justify-between mt-6 px-16 font-roboto">
+                <div className="md:col-span-9 grid grid-rows-1 justify-center p-4 md:mx-6 xs:mx-3">
+                    <section id="newsHeader" className="grid grid-rows-1 justify-center p-6">
                         <div className="text-justify">
                             <h1 className="text-4xl font-semibold">{newsDetail !== null ? newsDetail.header?.title : ""}</h1>
                             <p className="text-md my-3">{newsDetail !== null ? "by "+newsDetail.header?.editor + " | " + newsDetail.header?.date: ""}</p>
@@ -54,20 +54,20 @@ function NewsDetailPage(props) {
                         <div className="mt-6 p-0 flex justify-center">
                             <img
                                 className="block max-w-full h-auto news-header"
-                                src={newsDetail !== null ? `/asset/${newsDetail.img}` : `/asset/news_cartoon.jpg`}
+                                src={newsDetail !== null ? `${newsDetail.img}` : `/asset/news_cartoon.jpg`}
                             />
                         </div>
                     </section>
-                    <section id="newsContent" className="mx-8 p-6 shadow-lg rounded-lg drop-shadow-lg text-gray-500 text-center">
+                    <section id="newsContent" className="mx-8 p-6 rounded-lg text-gray-500 text-center">
                         {/*<h2 className="font-semibold text-3xl mb-5">Hello world!</h2>*/}
                         <hr className="my-6 border-gray-300" />
                         <p className="text-md text-justify">{newsDetail !== null ? newsDetail.body : ""}</p>
                     </section>
                 </div>
-                <div className="xl:col-span-2 md:col-span-3 md:mx-6 xs:mx-2 p-4">
-                    <h3 className="text-xl text-center">Berita Terbaru</h3>
+                <div className="md:col-span-3 md:mx-6 xs:mx-2 pt-2 mt-10">
+                <h3 className="w-full mb-10 font-bold text-md realtive pt-[20px] pr-auto pb-[15px] border-b-4 border-b-[#f2c808] border-solid text-[#103968]">Berita Terbaru</h3>
                     <div className="">
-                        <AutoScroll height={500} scrollBehavior={`smooth`} optionText={`Auto scroll`}>
+                        <AutoScroll height={800} scrollBehavior={`smooth`} showOption={false}>
                         {
                             Array.isArray(newsNewest) ?
                                 newsNewest.map((value, idx) => {
